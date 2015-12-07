cee0bb3888f155411523ac08baacc0bdc8f73af5
Richard Ward <richard@richard.ward.name>
2015-12-07 21:40:08 +0000

    Day 2 (part 2)

--- src/main/scala/advent/day2/Day2.scala
+++ src/main/scala/advent/day2/Day2.scala
@@ -5,19 +5,27 @@ import advent.Day
 object Day2 extends Day {
   val day = 2
 
-  val paper = loadInput().lines
-    .map(parseBox)
-    .map(dims =>
+  val boxes = loadInput().lines.map(parseBox).toSeq
+
+  val paper = boxes.map(dims =>
       (dims(0) * dims(1) * 2
         + dims(1) * dims(2) * 2
         + dims(0) * dims(2) * 2
-        + dims.sortWith(_<_).slice(0, 2).product
+        + shortestTwoSides(dims).product
         ))
     .sum
 
-  println(paper)
+  println("Paper required: %d".format(paper))
 
-  private def parseBox(line: String) = {
-    line.split('x').map(_.toInt)
-  }
+
+  val ribbon = boxes.map(dims =>
+    (shortestTwoSides(dims).sum * 2
+      + dims.product)
+  ).sum
+
+  println("Ribbon required: %d".format(ribbon))
+
+  private def parseBox(line: String) = line.split('x').map(_.toInt)
+
+  private def shortestTwoSides(dims: Array[Int]): Array[Int] = dims.sortWith(_ < _).slice(0, 2)
 }
